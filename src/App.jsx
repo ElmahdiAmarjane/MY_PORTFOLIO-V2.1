c4891d2b8219bf7ecf62e4df5c077e929eb8ce88
amarjane <amarjaneelmahdi03@gmail.com>
2024-03-20 18:23:12 +0000

    rsv1

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,40 @@ import Home from "./components/home/Home.tsx";
 import Slidebar from "./components/slideBar/Slidebar.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
+import { useEffect , useState } from "react";
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY < window.innerHeight;
+      setIsScrolled(scrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Empty dependency array means the effect will only run once after the initial render
+
 
   return (
-    <div className="appall " >
+    <div className="appall ">
       <div className="appcontainer">
         <div className="Slidebardiv">
           <Slidebar />
-        </div> 
-        <div id="home" className="homediv" >
+        </div>
+        <div id="home" className="homediv">
           <Home />
         </div>
-       
       </div>
+      {isScrolled ? null : (
       <div className="navbardiv">
         <Navbar />
-        </div>
+      </div> )}
       <div id="career" className="myprojectsdiv">
         <Career />
       </div>
@@ -32,7 +49,6 @@ function App() {
         <Footer />
       </div>
     </div>
-    
   );
 }
 
